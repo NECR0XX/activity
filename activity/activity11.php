ecaa2e733ba73463f5adf33d78d2068250ae331f
NECR0XX <carronananiasx@gmail.com>
2023-10-30 08:29:22 -0300

    a

--- activity/activity11.php
+++ activity/activity11.php
@@ -11,7 +11,7 @@ require_once '../app/Controller/AtivController.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A</title>
+    <title>Atividade</title>
 </head>
 <body>
 <button><a href="../index.html">Página Inicial</a></button><br><br>
--- activity/activity17.php
+++ activity/activity17.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A</title>
+    <title>Atividade</title>
 </head>
 <body>
 <button><a href="../index.html">Página Inicial</a></button><br><br>
